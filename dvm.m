6ae21ba5938d351486fe46c66c7fd64f64625f28
Pol Mesalles <pol.developer1@gmail.com>
2016-12-09 18:36:46 +0100

    Got rid of all them fors!
    And also removed unnecessary code.

--- dvm.m
+++ dvm.m
@@ -1,5 +1,5 @@
 clear variables;
-% clc;
+clc;
 
 airfoil = input('4-digit NACA: ','s');
 if (length(airfoil) ~= 4)
@@ -14,6 +14,7 @@ alpha = input('Angle of attack (º): ');
 if (alpha < -10 || alpha > 10)
     warning('Thin Airfoil Theory may not provide accurate results for the chosen angle of attack');
 end
+alpha = deg2rad(alpha);
 
 M = input('Number of panels: ');
 if (M <= 0)
@@ -46,7 +47,7 @@ if (flap == 'y' || flap == 'Y')
         error('Invalid hinge position');
     end
     
-    eta = input('Flap deflection angle (º): ');
+    eta = deg2rad(input('Flap deflection angle (º): '));
 else
     flap = false;
 end
@@ -70,7 +71,7 @@ if (flap)
     
     z_flap = zeros(1,N);
     reg3 = (x >= x_h);
-    z_flap(reg3) = -tan(deg2rad(eta)) * (x(reg3) - x_h);
+    z_flap(reg3) = -tan(eta) * (x(reg3) - x_h);
     
     z = z + z_flap;
 end
@@ -86,52 +87,24 @@ delta_x = x2 - x1;
 delta_z = z2 - z1;
 
 % Panel chords (length)
-c = sqrt((x2 - x1).^2 + (z2 - z1).^2);
-
-% Tangent vectors
-t = zeros(2,M);
-t(1,:) = (x2 - x1) ./ c; % x's
-t(2,:) = (z2 - z1) ./ c; % z's
+c = sqrt(delta_x.^2 + delta_z.^2);
 
 % Normal vectors
 n = zeros(2,M);
-n(1,:) = -t(2,:); % x's
-n(2,:) = t(1,:); % z's
+n(1,:) = -delta_z ./ c; % x's
+n(2,:) = delta_x ./ c; % z's
 
 % Vortex points (aerodynamic centers)
 VP = zeros(2,M);
-VP(1,:) =  x1 + (x2 - x1) * 1/4; 
-VP(2,:) =  z1 + (z2 - z1) * 1/4; 
+VP(1,:) =  x1 + delta_x * 1/4; 
+VP(2,:) =  z1 + delta_z * 1/4; 
 
 % Control points
 CP = zeros(2,M);
-CP(1,:) =  x1 + (x2 - x1) * 3/4; 
-CP(2,:) =  z1 + (z2 - z1) * 3/4; 
-
-% System of equations
-tic
-RHS = zeros(1,M);
-RHS_alpha = zeros(1,M);
-A = zeros(M);
-A_alpha = A;
-for i = 1:M % CP's
-    for j = 1:M % vortices
-        % Induced velocity at CP (i) due to vortex (j)
-        r_sq = (CP(1,i) - VP(1,j))^2 + (CP(2,i) - VP(2,j))^2;
-        u = 1/(2*pi) * (CP(2,i) - VP(2,j)) / r_sq;
-        w = -1/(2*pi) * (CP(1,i) - VP(1,j)) / r_sq;
-        A(i,j) = [u w] * n(:,i);
-        
-        r_sq_alpha = (CP(1,i) - VP(1,j))^2;
-        A_alpha(i,j) = -1/(2*pi) * (CP(1,i) - VP(1,j)) / r_sq_alpha; % * 1
-    end
-    RHS(i) = -n(1,i);
-    RHS_alpha(i) = -sin(deg2rad(alpha));
-end
-toc
+CP(1,:) =  x1 + delta_x * 3/4; 
+CP(2,:) =  z1 + delta_z * 3/4; 
 
 % Induced velocity at control point (i) due to vortex (j)
-tic
 in = combvec(1:M,1:M); % i's and j's
 
 % Airfoil (and flap) without angle of attack
@@ -144,22 +117,18 @@ r_sq_alphae = (CP(1,in(2,:)) - VP(1,in(1,:))).^2;
 v_alpha = -1/(2*pi) * (CP(1,in(2,:)) - VP(1,in(1,:))) ./ r_sq_alphae;
 
 % Systems of equations
-Ae = reshape(sum(v .* n(:,in(2,:))),[M M])';
-RHSe = -n(1,:);
-A_alphae = reshape(v_alpha,[M M])';
-RHS_alphae(1:M) = -sin(deg2rad(alpha));
-toc
+A = reshape(sum(v .* n(:,in(2,:))),[M M])';
+RHS = -n(1,:);
+A_alpha = reshape(v_alpha,[M M])';
+RHS_alpha(1:M) = -sin(alpha);
 
 % Circulation
-gamma = linsolve(A,RHS');
-gammae = linsolve(Ae,RHSe');
-gamma_alpha = linsolve(A_alpha,RHS_alpha');
-gamma_tot = gamma + gamma_alpha; % sum linsolves
+gamma = linsolve(A,RHS') + linsolve(A_alpha,RHS_alpha'); % superposition
 
 % Results
-Cl = 2 * sum(gamma_tot);
-Cm_LE = -2 * sum(gamma_tot' .* VP(1,:) * cos(deg2rad(alpha)));
-Cm_AC = -2 * sum(gamma_tot' .* (VP(1,:) - 1/4) * cos(deg2rad(alpha)));
+Cl = 2 * sum(gamma);
+Cm_LE = -2 * sum(gamma' .* VP(1,:) * cos(alpha));
+Cm_AC = -2 * sum(gamma' .* (VP(1,:) - 1/4) * cos(alpha));
 
 % Plots
 % plot(x, z); % mean camber line discretization
