6cabc4d0054cb734d2911347a9ebf9d473e7f35b
Pol Mesalles <pol.developer1@gmail.com>
2016-12-09 18:25:53 +0100

    Finally found an efficient solution!

--- dvm.m
+++ dvm.m
@@ -38,9 +38,14 @@ else
 end
 
 flap = input('Want a flap? [y/n] ','s');
-if (flap == 'y')
+if (flap == 'y' || flap == 'Y')
     flap = true;
-    x_h = input('Flap hinge location (% chord): ') / 100;
+    
+    x_h = input('Flap hinge location (in tenths of chord): ') / 10;
+    if (x_h > 10)
+        error('Invalid hinge position');
+    end
+    
     eta = input('Flap deflection angle (º): ');
 else
     flap = false;
@@ -52,7 +57,7 @@ if (p > 0)
     reg1 = (x <= p);
     z(reg1) = f/(p^2) * (2*p*x(reg1) - x(reg1).^2);
 
-    reg2 = (p < x);
+    reg2 = (x > p);
     z(reg2) = f/((1-p)^2) * (1 - 2*p + 2*p*x(reg2) - x(reg2).^2);
 else
     z(:) = 0; % flat plate
@@ -60,13 +65,13 @@ end
 
 % Flap
 if (flap)
-    z_flap = zeros(1,N);
     [~,x_h] = min(abs(x_h-x));
     x_h = x(x_h);
     
-    reg3 = x >= x_h;
+    z_flap = zeros(1,N);
+    reg3 = (x >= x_h);
     z_flap(reg3) = -tan(deg2rad(eta)) * (x(reg3) - x_h);
-    plot(x,z_flap);
+    
     z = z + z_flap;
 end
 
@@ -104,8 +109,11 @@ CP(1,:) =  x1 + (x2 - x1) * 3/4;
 CP(2,:) =  z1 + (z2 - z1) * 3/4; 
 
 % System of equations
+tic
 RHS = zeros(1,M);
+RHS_alpha = zeros(1,M);
 A = zeros(M);
+A_alpha = A;
 for i = 1:M % CP's
     for j = 1:M % vortices
         % Induced velocity at CP (i) due to vortex (j)
@@ -113,27 +121,45 @@ for i = 1:M % CP's
         u = 1/(2*pi) * (CP(2,i) - VP(2,j)) / r_sq;
         w = -1/(2*pi) * (CP(1,i) - VP(1,j)) / r_sq;
         A(i,j) = [u w] * n(:,i);
+        
+        r_sq_alpha = (CP(1,i) - VP(1,j))^2;
+        A_alpha(i,j) = -1/(2*pi) * (CP(1,i) - VP(1,j)) / r_sq_alpha; % * 1
     end
-    RHS(i) = -(cos(deg2rad(alpha)) * n(1,i) + sin(deg2rad(alpha)) * n(2,i));
+    RHS(i) = -n(1,i);
+    RHS_alpha(i) = -sin(deg2rad(alpha));
 end
+toc
 
-% Induced velocity at CP due to vortex
-% v = zeros(2,M);
-% for i = 1:M % CP's
-%     r_sq = (CP(1,i) - VP(1,:)).^2 + (CP(2,i) - VP(2,:)).^2;
-%     v(1,:) = (CP(2,i) - VP(2,:)) ./ r_sq;
-%     v(2,:) = -(CP(1,i) - VP(1,:)) ./ r_sq;
-%     v = 1/(2*pi) * v;
-% end
-% AA = v' * n;
-% RHSS = -[cos(deg2rad(alpha)) sin(deg2rad(alpha))] * n;
+% Induced velocity at control point (i) due to vortex (j)
+tic
+in = combvec(1:M,1:M); % i's and j's
 
+% Airfoil (and flap) without angle of attack
+r_sq = (CP(1,in(2,:)) - VP(1,in(1,:))).^2 + (CP(2,in(2,:)) - VP(2,in(1,:))).^2;
+v(1,:) = 1/(2*pi) * (CP(2,in(2,:)) - VP(2,in(1,:))) ./ r_sq;
+v(2,:) = -1/(2*pi) * (CP(1,in(2,:)) - VP(1,in(1,:))) ./ r_sq;
+
+% Flat plate with angle of attack
+r_sq_alphae = (CP(1,in(2,:)) - VP(1,in(1,:))).^2;
+v_alpha = -1/(2*pi) * (CP(1,in(2,:)) - VP(1,in(1,:))) ./ r_sq_alphae;
+
+% Systems of equations
+Ae = reshape(sum(v .* n(:,in(2,:))),[M M])';
+RHSe = -n(1,:);
+A_alphae = reshape(v_alpha,[M M])';
+RHS_alphae(1:M) = -sin(deg2rad(alpha));
+toc
+
+% Circulation
 gamma = linsolve(A,RHS');
+gammae = linsolve(Ae,RHSe');
+gamma_alpha = linsolve(A_alpha,RHS_alpha');
+gamma_tot = gamma + gamma_alpha; % sum linsolves
 
 % Results
-Cl = 2 * sum(gamma);
-Cm_LE = -2 * sum(gamma' .* VP(1,:) * cos(deg2rad(alpha)));
-Cm_AC = -2 * sum(gamma' .* (VP(1,:) - 1/4) * cos(deg2rad(alpha)));
+Cl = 2 * sum(gamma_tot);
+Cm_LE = -2 * sum(gamma_tot' .* VP(1,:) * cos(deg2rad(alpha)));
+Cm_AC = -2 * sum(gamma_tot' .* (VP(1,:) - 1/4) * cos(deg2rad(alpha)));
 
 % Plots
 % plot(x, z); % mean camber line discretization
